fac169fcab4f73e80d285d6a824d8d1b7b052276
Chubak Bidpaa <chubakbidpaa@gmail.com>
2022-11-15 15:01:13 +0330

    initialized and string utils added

---
+++ include/kaibash.h
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <string.h>
+#include <msquic.h>
+#include <glib.h>
+
+#define _GNU_SOURCE //asks stdio.h to include asprintf
+#include <stdio.h>
+
+#include "kquic.h"
+#include "fstr.h"
+#include "string_utilities.h"
